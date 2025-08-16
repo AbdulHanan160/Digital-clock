7e0078103533a491a2b51d17fbfedb0856ecf3e6
Abdul Hanan <hannan160160@gmail.com>
2025-08-16 14:56:31 +0500

    Remove unnecessary lines

--- script.js
+++ script.js
@@ -2,25 +2,25 @@ let mins = document.querySelector(".minutes p")
 let hour = document.querySelector(".hours p")
 let secs = document.querySelector(".seconds p")
 let session = document.querySelector(".sessions p")
-let hours =document.querySelector(".hours")
-let minutes =document.querySelector(".minutes")
-let seconds =document.querySelector(".seconds")
+let hours = document.querySelector(".hours")
+let minutes = document.querySelector(".minutes")
+let seconds = document.querySelector(".seconds")
+let date = document.querySelector(".date p")
 const showtime = () => {
     let now = new Date
-    console.log(now)
     let h = now.getHours()
     let m = now.getMinutes()
     let s = now.getSeconds()
-    let ms=now.getMilliseconds()
-  
+
+
     hour.innerHTML = h
     mins.innerHTML = m
     secs.innerHTML = s
-    if(h===0){
-        h=12
+    if (h === 0) {
+        h = 12
     }
     if (h > 12) {
-        h= h-12
+        h = h - 12
         session.innerHTML = "PM"
     }
     if (h < 10) {
@@ -32,16 +32,15 @@ const showtime = () => {
     if (s < 10) {
         secs.innerHTML = `0${s}`
     }
-    let radius_sec=((s+ms/1000)/60)*360
-    let radius_min=(m/60)*360
-    let radius_hour=(h/24)*360
-    seconds.style.background=`conic-gradient(rgb(51, 42, 231) ${radius_sec}deg, rgb(51, 42, 231) 0deg, white ${radius_sec}deg)`
-    minutes.style.background=`conic-gradient(rgb(244, 237, 13) ${radius_min}deg, rgb(244, 237, 13) 0deg, white ${radius_min}deg)`
-    hours.style.background=`conic-gradient(red ${radius_hour}deg, red 0deg, white ${radius_hour}deg)`
-    
+    let radius_sec = (s / 60) * 360
+    let radius_min = (m / 60) * 360
+    let radius_hour = (h / 12) * 360
+    seconds.style.background = `conic-gradient(rgb(51, 42, 231) ${radius_sec}deg, rgb(51, 42, 231) 0deg, white ${radius_sec}deg)`
+    minutes.style.background = `conic-gradient(rgb(244, 237, 13) ${radius_min}deg, rgb(244, 237, 13) 0deg, white ${radius_min}deg)`
+    hours.style.background = `conic-gradient(red ${radius_hour}deg, red 0deg, white ${radius_hour}deg)`
+
     requestAnimationFrame(showtime)
 }
 
-// setInterval(showtime, 1000)
-
 showtime()
+
